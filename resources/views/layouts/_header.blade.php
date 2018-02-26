5ce2c3f238f4f705269c801cf70a5c9e4b022e47
vagrant <vagrant@homestead>
2018-02-26 06:01:47 +0000

    Finish user auth

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -25,8 +25,31 @@
 			<!-- Right Side of Navbar -->
 			<ul class="nav navbar-nav navbar-right">
 				<!-- Authentication Link -->
-				<li><a href="#">登录</a></li>
-				<li><a href="#">注册</a></li>
+				@guest
+
+					<li><a href="{{ route('login') }}">登录</a></li>
+					<li><a href="{{ route('register') }}">注册</a></li>
+
+				@else 
+					<li class="dropdown">
+						<a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="dropdown-toggle">
+							<span class="user-avatar pull-left" style="margin-right:8px;margin-top:-5px;">
+								<img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" alt="" class="img-responsive img-circle" width="30px" height="30px">
+							</span>
+							{{ Auth::user()->name }} <span class="caret"></span>
+						</a>
+
+						<ul class="dropdown-menu" role='menu'>
+							<li>
+								<a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
+							</li>
+
+							<form action="{{ route('logout') }}" method="POST" id="logout-form" style="display: none;">
+								{{ csrf_field() }}
+							</form>
+						</ul>
+					</li>
+				@endguest
 			</ul>
 		</div>
 	</div>
