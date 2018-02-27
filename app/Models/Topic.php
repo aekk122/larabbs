a356bc9317d03f269661441d7cd52c9b60a21ead
vagrant <vagrant@homestead>
2018-02-27 04:01:48 +0000

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,12 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function belongsToCategory() {
+    	return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function belongsToUser() {
+    	return $this->belongsTo(User::class, 'user_id');
+    }
 }
---
+++ resources/views/topics/_topic_list.blade.php
@@ -0,0 +1,49 @@
+@if (count($topics))
+	<ul class="media-list">
+		@foreach ($topics as $topic)
+			<li class="media">
+				<div class="media-left">
+					<a href="{{ route('users.show', $topic->user_id) }}">
+						<img src="{{ $topic->belongsToUser->avatar }}" style="width:52px;height: 52px;" alt="" class="media-object img-thumbnail" title="{{ $topic->belongsToUser->name }}">
+					</a>
+				</div>
+
+				<div class="media-body">
+					
+					<div class="media-heading">
+						<a href="{{ route('topics.show', $topic->id) }}" title="{{ $topic->title }}">
+							{{ $topic->title }}
+						</a>
+						<a href="{{ route('topics.show', $topic->id) }}" class="pull-right">
+							<span class="badge">{{ $topic->reply_count }}</span>
+						</a>
+					</div>
+
+					<div class="media-body meta">
+						<a href="#" title="{{ $topic->belongsToCategory->name }}">
+							<span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
+							{{ $topic->belongsToCategory->name}}
+						</a>
+
+						<span>•</span>
+
+						<a href="{{ route('users.show', $topic->user_id) }}" title="{{ $topic->belongsToUser->name }}">
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+							{{ $topic->belongsToUser->name }}
+						</a>
+						<span>•</span>
+						<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+						<span class="timestamp" title="最后活跃于">{{ $topic->updated_at->diffForHumans() }}</span>
+					</div>
+				</div>
+			</li>
+
+			@if ( !$loop->last)
+				<hr>
+			@endif
+
+		@endforeach
+	</ul>
+@else 
+	<div class="empty-block">暂无数据 。。</div>
+@endif
