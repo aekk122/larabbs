4dba00858fd45e3bc18311d5a1bf82ae4ff9645c
vagrant <vagrant@homestead>
2018-02-27 06:11:30 +0000

    List topics created by user

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -36,7 +36,11 @@
 		{{-- 用户发布的内容 --}}
 		<div class="panel panel-default">
 			<div class="panel-body">
-				凑合着看吧。
+				<ul class="nav nav-tabs">
+					<li class="active"><a href="#">Ta 的话题</a></li>
+					<li><a href="#">Ta 的回复</a></li>
+				</ul>
+				@include('users._topics', ['topics' => $user->hasManyTopics()->recent()->paginate(5)])
 			</div>
 		</div>
 	</div>
