3fb8743f0df6332b538b417863d22b5303dd7b9e
vagrant <vagrant@homestead>
2018-04-13 10:17:17 +0000

    api/user

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
 	'namespace' => 'App\Http\Controllers\Api',
+	'middleware' => 'serializer:data', // 切换返回数据结构
 ], function ($api) {
 	
 	$api->group([
@@ -56,5 +57,21 @@ $api->version('v1', [
 		// 删除 Token
 		$api->delete('authorizations/current', 'AuthorizationsController@destroy')
 			->name('api/authorizations.destroy');
+	});
+
+	$api->group([
+		'middleware' => 'api.throttle',
+		'limit' => config('api.rate_limits.access.limit'),
+		'expiers' => config('api.rate_limits.access.expires'),
+	], function ($api) {
+		// 游客可以访问的接口
+
+
+		// 需要 Token 验证访问的接口
+		$api->group(['middleware' => 'api.auth'], function ($api) {
+			// 当前登录用户信息
+			$api->get('user', 'UsersController@me')
+				->name('api.user.show');
+		});
 	});
 });
