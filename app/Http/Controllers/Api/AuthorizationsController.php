4d164f33185c0b796d533cd8002d9327b60ebbbe
vagrant <vagrant@homestead>
2018-05-02 10:17:08 +0000

    social login chagne to passport

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -11,9 +11,13 @@ use Zend\Diactoros\Response as Psr7Response;
 use Psr\Http\Message\ServerRequestInterface;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use League\OAuth2\Server\AuthorizationServer;
+use App\Traits\PassportToken;   
 
 class AuthorizationsController extends Controller
 {
+
+    use PassportToken;
+
 	protected $allow_social = ['weixin'];
 
     public function socialStore($social_type, SocialAuthorizationRequest $request) {
@@ -64,9 +68,11 @@ class AuthorizationsController extends Controller
     			break;
     	}
 
-        $token = Auth::guard('api')->fromUser($user);
+        // $token = Auth::guard('api')->fromUser($user);
 
-        return $this->responseWithToken($token)->setStatusCode(201);
+        $result = $this->getBearerTokenByUser($user, '1', false);
+
+        return $this->response->array($result)->setStatusCode(201);
     }
 
     public function store(AuthorizationRequest $originRequest, AuthorizationServer $server, ServerRequestInterface $serverRequest) {
